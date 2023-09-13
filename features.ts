a95bd352d289aa61ce67ea46bdea676d6dce888e
Abhishek Shandilya <97976470+abhishandilya@users.noreply.github.com>
2023-09-13 00:45:59 -0400

    move out features

---
+++ features.ts
@@ -0,0 +1,114 @@
+export const FEATURE_LIST: { done: boolean; name: string }[] = [
+  {
+    name: "bun access",
+    done: false,
+  },
+  {
+    name: "bun adduser",
+    done: false,
+  },
+  {
+    name: "bun audit",
+    done: false,
+  },
+  {
+    name: "bun bugs",
+    done: false,
+  },
+  {
+    name: "bun cache",
+    done: false,
+  },
+  {
+    name: "bun ci",
+    done: false,
+  },
+  {
+    name: "bun completion",
+    done: false,
+  },
+  {
+    name: "bun config",
+    done: false,
+  },
+  {
+    name: "bun dedupe",
+    done: false,
+  },
+  {
+    name: "bun deprecate",
+    done: false,
+  },
+  {
+    name: "bun diff",
+    done: false,
+  },
+  {
+    name: "bun dist-tag",
+    done: false,
+  },
+  {
+    name: "bun docs",
+    done: false,
+  },
+  {
+    name: "bun doctor",
+    done: false,
+  },
+  {
+    name: "bun edit",
+    done: false,
+  },
+  {
+    name: "bun exec",
+    done: false,
+  },
+  {
+    name: "bun explain",
+    done: false,
+  },
+  {
+    name: "bun explore",
+    done: false,
+  },
+  {
+    name: "bun find-dupes",
+    done: false,
+  },
+  {
+    name: "bun fund",
+    done: false,
+  },
+  {
+    name: "bun get",
+    done: false,
+  },
+  {
+    name: "bun help",
+    done: true,
+  },
+  {
+    name: "bun install",
+    done: true,
+  },
+  {
+    name: "bun link",
+    done: true,
+  },
+  {
+    name: "bun run",
+    done: true,
+  },
+  {
+    name: "bun remove",
+    done: true,
+  },
+  {
+    name: "bun test",
+    done: true,
+  },
+  {
+    name: "bun unlink",
+    done: true,
+  },
+];
